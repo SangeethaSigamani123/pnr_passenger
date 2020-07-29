ae97d40dfd8c160a7162b87b52b995d57e382196
SangeethaSigamani123 <65025507+SangeethaSigamani123@users.noreply.github.com>
2020-07-29 18:15:06 +0530

    Update App.java

--- PnrProject/src/main/java/com/hexa/PnrProject/App.java
+++ PnrProject/src/main/java/com/hexa/PnrProject/App.java
@@ -6,6 +6,12 @@ package com.hexa.PnrProject;
  */
 public class App 
 {
+    
+    public int result(){
+        int a=1,b=1;
+        c= a+b;
+        return c;
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
